a9457d958a55c976be124c845f274e5547ba6379
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-03-09 13:39:55 -0500

    🚀 Galeria proyectos v1

--- pages/proyectos/index.vue
+++ pages/proyectos/index.vue
@@ -1,23 +1,45 @@
 <template>
   <div>
     <PaginaCargando v-if="$fetchState.pending" />
-    <Error v-else-if="$fetchState.error" />
+    <PaginaError v-else-if="$fetchState.error" />
 
     <template v-else>
-      <h1>{{ pagina.titulo }}</h1>
-      <p>{{ pagina.contenido }}</p>
+      <section class="contenido inicioPagina">
+        <h1 class="tituloPagina">{{ pagina.titulo }}</h1>
+        <div v-html="$md.render(pagina.contenido)"></div>
+      </section>
 
       <div class="contenedorProyectos">
-        <div v-for="proyecto in proyectos" :key="proyecto.id" class="proyecto">
-          <NuxtLink :to="`/proyectos/${proyecto.slug}`">
-            <img :src="img(proyecto.banner.id)" :alt="proyecto.banner.title" />
-          </NuxtLink>
-
-          <p class="nombre">
-            <NuxtLink :to="`/proyectos/${proyecto.slug}`">
-              {{ proyecto.titulo }}
+        <div v-for="(proyecto, i) in proyectos" :key="`proyecto${i}`" class="proyecto">
+          <div class="contenedorImg">
+            <NuxtLink class="enlaceImg" :to="`/proyectos/${proyecto.slug}`">
+              <img :src="img(proyecto.banner.id)" :alt="proyecto.banner.title" />
             </NuxtLink>
-          </p>
+          </div>
+
+          <section class="contenedorDescripcion">
+            <h2 class="titulo">
+              <NuxtLink :to="`/proyectos/${proyecto.slug}`">
+                {{ proyecto.titulo }}
+              </NuxtLink>
+            </h2>
+
+            <div class="seccionDescripcion">
+              <p>{{ proyecto.descripcion }}</p>
+            </div>
+
+            <div class="seccionDescripcion">
+              <a v-if="proyecto.enlace" class="enlace" :href="proyecto.enlace" target="_blank">Ver Proyecto</a>
+            </div>
+
+            <div class="seccionDescripcion repos">
+              <p class="interTitulo">Código:</p>
+              <div v-for="(repo, j) in proyecto.repos" :key="`repo-${j}`" class="repo">
+                <SvgIconosRedes nombre="github" abierto="false" />
+                <a class="repoNombre" :href="repo.url" target="_blank">{{ repo.nombre }}</a>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </template>
@@ -35,7 +57,6 @@ export default {
       proyectos: [],
     };
   },
-
   async fetch() {
     const query = gql`
       query {
@@ -53,7 +74,10 @@ export default {
           id
           titulo
           slug
+          descripcion
           fecha_publicacion
+          enlace
+          repos
           banner {
             id
             title
@@ -61,9 +85,7 @@ export default {
         }
       }
     `;
-
     const { paginas, proyectos } = await this.$graphql.principal.request(query);
-
     if (paginas.length && paginas[0].slug) {
       this.pagina = paginas[0];
     } else {
@@ -72,12 +94,10 @@ export default {
       }
       throw new Error('La página no existe');
     }
-
     if (proyectos && proyectos.length) {
       this.proyectos = proyectos;
     }
   },
-
   head() {
     return crearHead(
       this.$store.state.general.datos.nombre,
@@ -87,7 +107,6 @@ export default {
       this.$nuxt.$route.path
     );
   },
-
   methods: {
     img(imgId) {
       return urlImagen(imgId, {
@@ -100,4 +119,138 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.contenedorProyectos {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.proyecto {
+  display: flex;
+  flex-direction: column;
+  width: 95vw;
+  margin: 2em 0;
+}
+
+.contenedorImg {
+  img {
+    max-height: 100%;
+    max-width: 100%;
+    object-fit: cover;
+    vertical-align: top;
+  }
+}
+
+.contenedorDescripcion {
+  font-size: 0.8em;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1em;
+
+  border-bottom: 2px solid;
+
+  .titulo {
+    font-size: 1.1em;
+    margin-top: 0.8em;
+
+    a,
+    a:link {
+      color: black;
+      padding: 0.8em;
+      margin-bottom: 0.5em;
+      background-image: url(~/assets/imgs/marco-oscuro.svg);
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      transition: 0.2s ease-in-out opacity;
+
+      &:hover {
+        opacity: 0.6;
+      }
+    }
+  }
+
+  p {
+    margin: 1em 0;
+    line-height: 1.4;
+  }
+
+  .seccionDescripcion {
+    margin: 0.5em 0;
+  }
+}
+
+.enlace {
+  padding: 0.5em;
+  background-color: $colorPrincipal;
+  color: white;
+  width: 50%;
+  text-align: center;
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+
+.repos {
+  display: flex;
+
+  .interTitulo {
+    margin: 0 0.5em 0 0;
+  }
+
+  .repo {
+    margin-right: 1em;
+    display: flex;
+    align-items: center;
+  }
+
+  .iconoRed {
+    width: 15px;
+  }
+
+  .repoNombre {
+    margin-left: 0.3em;
+  }
+}
+
+// Teléfonos horizontal
+@media (min-width: $minCelular) {
+  .proyecto {
+    width: 80vw;
+  }
+}
+
+// Pantallas medianas (Tablets)
+@media (min-width: $minTablet) {
+  .proyecto {
+    flex-direction: row;
+    width: 95vw;
+  }
+
+  .contenedorImg {
+    width: 50%;
+  }
+
+  .contenedorDescripcion {
+    width: 50%;
+    margin-left: 1em;
+    margin-top: 0;
+    justify-content: space-between;
+  }
+}
+
+// Dispositivos grandes y pantallas medianas
+@media (min-width: $minPantalla) {
+  .proyecto {
+    width: 90vw;
+  }
+}
+
+// Pantallas grandes
+@media (min-width: $minPantallaGrande) {
+  .proyecto {
+    width: 70vw;
+  }
+}
+</style>
