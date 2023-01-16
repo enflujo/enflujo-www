6d3e21204e7b7ba339bab0d914b0985f985c29ec
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-01-16 15:56:49 -0500

    🔨 Prueba despliegue

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ export default {
     //
   },
 
-  beforeDestroy() {
+  beforeUnmount() {
     window.removeEventListener('resize', this.actualizar);
     window.cancelAnimationFrame(this.animReq);
   },
