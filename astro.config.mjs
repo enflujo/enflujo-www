29adbed53f769b12771484baa542be547a98ba5b
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-02-10 00:33:27 -0500

    👽️ Robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import graphql from '@rollup/plugin-graphql';
+import robotsTxt from 'astro-robots-txt';
 
+// https://astro.build/config
 export default defineConfig({
   outDir: './publico',
   publicDir: './recursos',
   site: 'https://enflujo.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), robotsTxt()],
   vite: {
     plugins: [graphql()],
   },
