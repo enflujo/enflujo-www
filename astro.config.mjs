b280f995f8b645ba0350ce283b1b2d100a9d2058
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-04-23 19:58:20 -0500

    💚 Comprimir archivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import graphql from '@rollup/plugin-graphql';
 import robotsTxt from 'astro-robots-txt';
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './publico',
   publicDir: './recursos',
   site: 'https://enflujo.com',
-  integrations: [sitemap(), robotsTxt()],
+  integrations: [sitemap(), robotsTxt(), compress({
+    path: './publico'
+  })],
   vite: {
-    plugins: [graphql()],
-  },
+    plugins: [graphql()]
+  }
 });
