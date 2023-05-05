0032f62a86c49dc3c002401256a50921af5fd8bd
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-05-05 10:02:03 -0500

    ✅ Tipos básicos para Astro

--- src/enflujo.d.ts
+++ src/enflujo.d.ts
@@ -1,1 +1,22 @@
-// Aquí no va nada
+export type PersonaEquipo = {
+  date_created: Date;
+  nombre: string;
+  slug: string;
+  descripcion: String;
+  activo: boolean;
+  redes: Red[];
+  sitio_web: string;
+  rol: String;
+  foto: Foto;
+};
+
+export type Red = {
+  red: string;
+  usuario: string;
+  url: string;
+};
+
+export type Foto = {
+  id: string;
+  title: string;
+};
