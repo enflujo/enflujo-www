e520825e8a59525fe5abc6bf696bf1feb3c3b69a
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-05-05 10:05:15 -0500

    🚨 resolver estilo código

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,21 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import graphql from '@rollup/plugin-graphql';
 import robotsTxt from 'astro-robots-txt';
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './publico',
   publicDir: './recursos',
   site: 'https://enflujo.com',
-  integrations: [sitemap(), robotsTxt(), compress({
-    path: './publico'
-  })],
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    compress({
+      path: './publico',
+    }),
+  ],
   vite: {
-    plugins: [graphql()]
-  }
+    plugins: [graphql()],
+  },
 });
